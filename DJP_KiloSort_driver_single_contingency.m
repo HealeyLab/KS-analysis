b97b19d28a221b3587a7b1276e92f1a3f0ab8068
HealeyLab <healeylab@gmail.com>
2020-11-17 16:25:13 -0500

    Renamed DJP drivers for contingencies

--- DJP_KiloSort_driver_single_contingency.m
+++ DJP_KiloSort_driver_single_contingency.m
@@ -1,9 +1,8 @@
 %% Author: DJP
-% 4/20/2020
 clear all;
 close all;
 
-%% Get files, take user input, and make folder
+%% Conversion
 [files, origDataPath] = ... % crucial distinction: files vs file (current)
     uigetfile('*.rhd', 'Select an RHD2000 Data File', 'MultiSelect', 'on');
 % [file,origDataPath] = concat_Intan_RHD2000_files;
@@ -15,25 +14,23 @@ else
     files = {files};
 end
 ts_label = ts_label(1:end-4);
+% if you are aggregating things for cell shape analysis, input depth,
+% otherwise click enter/return
 
-% Getting using input about whether you are actually using all 32 channels,
-% and whether you are using stereotrodes (as opposed to tetrodes)
 is_32 = input('is this a 32 channel recording? (''1'' for yes, ''0'' for no )\n');
 is_stereo = input('does the microdrive use stereotrodes? (''1'' for yes, ''0'' for no ) \n');
-
-% Naming the directory/folder that will be named here
-dataPath = fullfile(origDataPath,['DJPComb_' ts_label '_Kilosort']);
+    
+    
+dataPath = fullfile(origDataPath,[ts_label '_Kilosort']);
 mkdir(dataPath)
 addpath(dataPath)
-%% Build .dat, adc_data, and figure out when the context switches
-% Initialize adc data
-board_adc = [];
-
-% open raw.dat to write
+%%
 dataFileName = fullfile(dataPath, 'raw_filtered.dat'); % [dataPath '\Kilosort_alldata\raw.dat'] % make .dat file
-rawDataFID = fopen(dataFileName, 'w'); % open .dat file for writing
+% saving adc data, too
+board_adc = [];
+% open raw.dat to write
+fid1a = fopen(dataFileName, 'w'); % open .dat file for writing
 
-% sort according to filename. Not timestamp but filename
 filearray = [];
 for i = 1:length(files)
     filearray = [filearray dir(char(files(i)))];
@@ -42,45 +39,32 @@ end
 files = files(idx);
 
 % make a waitbar!
-
 f = waitbar(0, 'loading');
-% I need to keep track of how long the recording is so that when the fem
-% return is added, I can save the length. contextSwitch means the time at
-% which the combined recording I'm doing is 
-
 for i=1:length(files)
-    % read
+    
     read_Intan_RHD2000_file_MML_DJP(fullfile(filearray(i).folder,filearray(i).name),0);
     
     % bandpass filter
     datr = filter_datr(amplifier_data, frequency_parameters);
 
-    % append to .dat files
-    fwrite(rawDataFID, datr(:),'int16'); %     fwrite(fid1a, amplifier_data(:),'int16'); % append to .dat file
-    
-    % append to .mat file for adc_data
+    fwrite(fid1a, datr(:),'int16'); % append to .dat files
+    %     fwrite(fid1a, amplifier_data(:),'int16'); % append to .dat file
     if size(board_adc_data, 1) == 2
         board_adc_data = [NaN(1, size(board_adc_data, 2)); board_adc_data];
     end
     board_adc = [board_adc board_adc_data];
     
-
-    % write your file
-    fprintf(contingenciesFid, [filearray(i).name '\t' num2str(size(amplifier_data, 2)) '\n']);
+    waitbar(i/length(files), f, 'loading Intan data')
     
-    % update waitbar!
-    waitbar(i/length(files), waitbarFig, 'loading Intan data')
 end
 
-fclose(rawDataFID);
-fclose(contingenciesFid);
-close(waitbarFig); % close waitbar
-
+fclose(fid1a);
+close(f); % close waitbar
 
 adc_sr=frequency_parameters.board_adc_sample_rate;
 save(fullfile(dataPath, 'adc_data'), 'board_adc', 'adc_sr', '-v7.3')
-%% write params.py
-paramspyFid = fopen(fullfile(dataPath, 'params.py'),'w');
+%% make params.py
+fid2 = fopen(fullfile(dataPath, 'params.py'),'w');
 
 dat_path = 'raw_filtered.dat';
 n_channels_dat = string(length(amplifier_channels));
@@ -89,22 +73,22 @@ offset = 0;
 sample_rate = string(frequency_parameters.amplifier_sample_rate);
 hp_filtered = 'True';
 
-fprintf(paramspyFid, 'dat_path = r''%s''\n', dat_path);
-fprintf(paramspyFid, 'n_channels_dat = %s\n', n_channels_dat); %2d means two digit
-fprintf(paramspyFid, 'dtype = ''%s''\n', dtype);
-fprintf(paramspyFid, 'offset = 0\n');
-fprintf(paramspyFid, 'sample_rate = %s.\n', sample_rate);
-fprintf(paramspyFid, 'hp_filtered = %s\n', hp_filtered);
+fprintf(fid2, 'dat_path = r''%s''\n', dat_path);
+fprintf(fid2, 'n_channels_dat = %s\n', n_channels_dat); %2d means two digit
+fprintf(fid2, 'dtype = ''%s''\n', dtype);
+fprintf(fid2, 'offset = 0\n');
+fprintf(fid2, 'sample_rate = %s.\n', sample_rate);
+fprintf(fid2, 'hp_filtered = %s\n', hp_filtered);
 
-fclose(paramspyFid);
+fclose(fid2);
 clear fid2 dat_path dtype offset hp_filtered 
 clear amplifier_channels amplifier_data aux_input_channels aux_input_data ...
         board_dig_in_data board_dig_in_channels filename frequency_parameters ...
         notes reference_channel spike_triggers supply_voltage_channels supply_voltage_data ...
         t_amplifier t_aux_input t_dig t_supply_voltage
-%% Identify the right config files to copy in
+%% Figure out which standard config is neede dhere
 % copy master file example and  standard config and then edit them
-working_dir = 'C:\Users\HealeyLab\Documents\DJP\KS-analysis';
+working_dir = 'C:\Users\Healey Lab\Documents\MATLAB\KS-analysis';
 if      is_32 && ~is_stereo % if 32 chan tetrode
     ChannelMapFile_orig      = fullfile(working_dir, 'createChannelMapFile32.m');
 elseif ~is_32 &&  is_stereo % 16 chan stereotrode
@@ -133,18 +117,18 @@ end
 master_file_example_pasted = fullfile(dataPath, 'master_file_example_MOVEME.m');
 StandardConfig_pasted      = fullfile(dataPath, 'StandardConfig_MOVEME.m');
 
-%% Edit Standard Config File
+%% Edit standard config
 % https://www.mathworks.com/matlabcentral/answers/62986-how-to-change-a-specific-line-in-a-text-file
-standardConfigFid = fopen(StandardConfig_pasted,'r');
+fid3 = fopen(StandardConfig_pasted,'r');
 i = 1;
-tline = fgetl(standardConfigFid);
+tline = fgetl(fid3);
 A{i} = tline;
 while ischar(tline)
     i = i+1;
-    tline = fgetl(standardConfigFid);
+    tline = fgetl(fid3);
     A{i} = tline;
 end
-fclose(standardConfigFid);
+fclose(fid3);
 % Change lines of .m file
 A{1}  = sprintf('ops.GPU=1;');
 A{2}  = sprintf('ops.parfor=1;');
@@ -156,9 +140,11 @@ A{12} = sprintf('ops.NchanTOT = %s;', n_channels_dat);
 A{14} = sprintf('ops.Nfilt = 32;');
 A{47} = sprintf('ops.mergeT           = .08;');
 A{48} = sprintf('ops.splitT           = .12;');
+
+
 A{51} = sprintf('ops.initialize      = ''fromData'';');
 A{52} = sprintf('ops.spkTh           = -5;');
-%%
+
 if is_32
     num_active_chan = 32;
 else
@@ -173,17 +159,17 @@ A{16} = sprintf('ops.nNeigh = %s; % visualization only (Phy): number of neighbor
 A{24} = sprintf('ops.chanMap = ''%s'';',  fullfile(dataPath, 'chanMap.mat'));
 
 % Write cell A into txt
-standardConfigFid = fopen(StandardConfig_pasted, 'w');
+fid3 = fopen(StandardConfig_pasted, 'w');
 for i = 1:numel(A)
     if A{i+1} == -1
-        fprintf(standardConfigFid,'%s', A{i});
+        fprintf(fid3,'%s', A{i});
         break
     else
-        fprintf(standardConfigFid,'%s\n', A{i});
+        fprintf(fid3,'%s\n', A{i});
     end
 end
-fclose(standardConfigFid);
-%% Edit ChannelMap
+fclose(fid3);
+%% Edit Channelmap 
 fid4 = fopen(ChannelMapFile_pasted,'r');
 i = 1;
 tline = fgetl(fid4);
@@ -229,11 +215,9 @@ while ischar(tline)
     C{i} = tline;
 end
 fclose(fid5);
-% Change lines of .m file
 C{3} = sprintf('addpath(genpath(''C:\\KiloSort-master''))'); % path to LOCAL kilosort folder
 
 C{6} = sprintf('pathToYourConfigFile = ''%s''; ', dataPath);
-% C{24} = sprintf('rez = merge_posthoc2(rez);', dataPath);
 
 % Write cell C into txt
 fid5 = fopen(master_file_example_pasted, 'w');
